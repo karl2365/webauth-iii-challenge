1dff8d87b5bf0621a759e0077fe2a6cf77bf715b
karl2365 <karlbeksa@gmail.com>
2019-08-22 10:53:54 -0400

    :mute: removing console.logs

--- api/server.js
+++ api/server.js
@@ -15,27 +15,5 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
-server.get('/', (req, res) => {
-  res.send("It's alive!");
-});
-
-server.get('/token', (req, res) => {
-  // use jsonwebtoken library to produce a token
-  // add your name to the token's payload
-  const role = 'admin';
-  const payload = {
-    // subject is normally the user's id (who/what the token describes)
-    subject: 'me', // translates unto the "sub" property on the token
-    role,
-  };
-  const secret = 'is it secret, is it safe?';
-  const options = {
-    expiresIn: '8h',
-  };
-  const token = jwt.sign(payload, secret, options);
-
-  // return the token to the client
-  res.status(200).json({ role: role, token });
-});
-
+// 
 module.exports = server;
